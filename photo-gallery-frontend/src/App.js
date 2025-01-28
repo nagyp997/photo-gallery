30fb25ed73a0ba1c552d802ffe841f758eda79f0
Nagy Péter <nagyp97@gmail.com>
2025-01-28 12:08:01 +0100

    Add App.js

---
+++ photo-gallery-frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import Gallery from './components/Gallery';
+import Album from './components/Album';
+
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/gallery" element={<Gallery />} />
+                <Route path="/albums" element={<Album />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
