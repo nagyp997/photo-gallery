8e323d0985946411c85b5ccec6dcc803fc7419a1
Nagy Péter <nagyp97@gmail.com>
2025-01-28 11:37:50 +0100

    Add api.js

---
+++ photo-gallery-frontend/src/api/api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API = axios.create({ baseURL: 'http://localhost:5000' });
+
+API.interceptors.request.use(req => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        req.headers.Authorization = `Bearer ${token}`;
+    }
+    return req;
+});
+
+export const login = (formData) => API.post('/auth/login', formData);
+export const register = (formData) => API.post('/auth/register', formData);
+export const fetchImages = () => API.get('/images');
+export const createImage = (data) => API.post('/images/new', data);
